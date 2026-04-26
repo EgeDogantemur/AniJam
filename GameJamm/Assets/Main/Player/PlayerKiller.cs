9db914767bf3613359f414cd6b573e2d6b1a07b4
Umut-Ersoy <ue.umut.ersoy@gmail.com>
2026-04-26 14:06:54 +0300

    blablaları ekledik

---
+++ GameJamm/Assets/Main/Player/PlayerKiller.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerKiller : MonoBehaviour
+{
+    public string enemyTag = "Enemy";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if(other.CompareTag(enemyTag))
+        {
+            Debug.Log("Player died");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
